ae15e3220f7269ec4f7f679607596d7837e1b9b9
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2023-03-18 18:42:54 +0100

    Big changes
    - Add Screen Viewer LGato's project
    - Add Potions & Magic MathysCraft's project
    - Add a temporary projects page

--- cfwidget.js
+++ cfwidget.js
@@ -1,20 +1,37 @@
-/*Intégration du nombre de 
-  téléchargements grâce à l'API cfWidget*/
-
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET', "https://api.cfwidget.com/373274");
-
-xhr.onreadystatechange = function() { 
-
-  if (xhr.readyState === 4) {
-
-    const response = JSON.parse(xhr.responseText);
-    let downloads = response.downloads.total;
+// Intégration du nombre de téléchargements de Mineria grâce à l'API cfWidget
+fetch("https://api.cfwidget.com/373274")
+  .then(response => response.json())
+  .then(data => {
+    let downloads = data.downloads.total;
     document.querySelector("#download-count").textContent = downloads;
+  })
+  .catch(error => console.error(error));
 
-  }
+// Screenshot Viewer
+fetch("https://api.cfwidget.com/693961")
+  .then(response => response.json())
+  .then(data => {
+    let downloads = data.downloads.total;
+    document.querySelector("#sv-download-count").textContent = downloads;
+  })
+  .catch(error => console.error(error));
 
-};
+// avec l'api curse forge :
+// const headers = {
+//   'Accept':'application/json',
+//   'x-api-key':'$2a$10$c.c7ufzl0eLs9patkUY0d.FHZFSaOLfzmLfi6H4Lk59du7rroNJOu'
+// };
 
-xhr.send();
+// fetch('https://api.curseforge.com/v1/mods/373274',
+// {
+//   method: 'GET',
+
+//   headers: headers
+// })
+// .then(function(res) {
+//     return res.json();
+// }).then(function(body) {
+//     const downloads = body.data.downloadCount
+//     console.log(body)
+//     document.querySelector("#sv-download-count").textContent = downloads;
+// });
